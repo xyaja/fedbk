c820a6d4a0c67dc8d09261890a68d5a544c696b9
xyaja <163610653+xyaja@users.noreply.github.com>
2024-10-02 00:19:30 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+api_id = '29726374'       # Ganti dengan API ID Anda
+api_hash = 'ee797487083e78676bc682c2e78df5fc'   # Ganti dengan API Hash Anda
+bot_token = '7698480033:AAHx1VPz17-GpopHXYWup1ZrHKeL1XxkzY4' # Ganti dengan token bot Anda
+owner_id = 7526061771     # Ganti dengan ID owner Anda (dalam bentuk integer)
